65ff1ac7f4d6e78e19ce16b5847a92e41748bc40
Mater <88980654+Mater8600@users.noreply.github.com>
2024-03-12 00:53:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import json
 import requests 
 import time
+from collections import Counter
 #### Made by "all" ####
 ### Heavily inspired by Ruben sim ###
 
 print("""
-█▀█ █▀▀ █▀▄ █▀█ █▀▀ █ █▄░█ █▀▄ █▀▀ █▀█
-█▀▀ ██▄ █▄▀ █▄█ █▀░ █ █░▀█ █▄▀ ██▄ █▀▄
+█▀█ █▀█ █▀▀ █░░ ▄▀█ █▀▀ █▀▀ █▀▀ █▀█
+█▀▄ █▄█ █▀░ █▄▄ █▀█ █▄█ █▄█ ██▄ █▀▄
 """)
 print("""
 █▄▄ █▄█   ▄▀█ █░░ █░░
@@ -43,9 +43,10 @@ def main(id):
                 ids.append(entry['user']['userId'])
             next_page = response['nextPageCursor']
             print(next_page)
+            time.sleep(2)
         except:
             print("no such page exists, going forward without any extra :(")
-        time.sleep(2)
+        
             
 
     print("\nList of names:\n\n")
@@ -56,9 +57,9 @@ def main(id):
     werdios = []
     names_of_werdios = []
     for i in display_names_list:
+        ### Yeah, searching is fun!!! ## 
         
-        
-        if "bbc" in str(i) or "czm" in str(i) or "czmdump" in str(i) or "bunny" in str(i) or "bun" in str(i)  or "fill" in str(i)  or "sus" in str(i) or "doll" in str(i) or "love" in str(i) or "bxnny" in str(i):
+        if "bbc" in str(i) or "czm" in str(i) or "czmdump" in str(i) or "bunny" in str(i) or "bun" in str(i)  or "fill" in str(i)  or "sus" in str(i) or "doll" in str(i) or "love" in str(i) or "bxnny" in str(i) or "bull" in str(i) or "bxll" in str(i) or "luv" in str(i) or "bulls" in str(i) or "buIIs" in str(i) or "buII" in str(i):
             print("found a werid username INVESTIGATE BEFORE REPORTING THESE ACCOUNTS!!!")
             werdios.append(ids[display_names_list.index(i)])
             names_of_werdios.append(i)
@@ -66,14 +67,46 @@ def main(id):
             print(names_of_werdios)
             print("full list (ids):\n")
             print(werdios)
+       
+    print("this will take some time!")
+    werdios_groups = []
+    werdios_groups_names = []
+    for i in werdios:
+            print("Let's get these guy's groups and compare them to each others joined ones.")
+            response_groups = requests.get(f"https://groups.roblox.com/v1/users/{i}/groups/roles?includeLocked=true")
+            response_groups = response_groups.json()
+            for entry in response_groups['data']:
+                werdios_groups.append(entry['group']['id'])
+                werdios_groups_names.append(entry['group']['name'])
+                
+                print("werdios groups:\n")
+                print(werdios_groups_names)
+                print("werdios group ids")
+                print(werdios_groups)
+          
+    group_id_count = Counter(werdios_groups)
+    
+    print("\n\n\nGroup counts\n")
+    print(group_id_count)
+        
+    print("Comparing rn...")
+        
+    
+        
+    print("Gottem, found these groups!")
+    common = group_id_count.most_common(2)
+    print(common)
             
-             
+       
+            
+            
+    
 
 if __name__ == "__main__":
-    id = input("Enter the id: ")
+    url = input("Enter the id: ")
   
         
-    main(id)
+    main(url)
         
        
     print("Done")

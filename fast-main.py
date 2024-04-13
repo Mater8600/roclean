78f5b0ae9ed6cbb8055c3ac052cee6ccb4358428
Mater <88980654+Mater8600@users.noreply.github.com>
2024-04-13 00:14:46 -0300

    Update fast-main.py
    ADDED more werdios names

--- fast-main.py
+++ fast-main.py
@@ -19,7 +19,7 @@ pages = 1000
 ### the list of "sus" words ###
 list_of_common_usernames = ["bbc", "czm", "czmdump", "bunny", "bun", "fill", "sus", "doll", "Bawls",
                             "bxnny", "bull", "bxll", "luv", "bulls", "buIIs", "buII", "hearts", "Hearts",
-                             "12yr", "cxm", "ass", "a33", "fap", "reps"]
+                             "12yr", "cxm", "ass", "a33", "fap", "reps", "blow", "m1kies"]
 
 print("""
 █▀█ █▀█ █▀▀ █░░ ▄▀█ █▀▀ █▀▀ █▀▀ █▀█
